3cc58b2c003758cf7d9331f732b2e2273fbd96a9
Jannis Achstetter <kripton@kripserver.net>
2021-04-11 22:14:05 +0200

    Include RF24, RF24Network and RF24Mesh by using their CMakeLists.txt files (that currently exist only in my kripton-rp2xxx branches)

--- src/wireless.cpp
+++ src/wireless.cpp
@@ -3,9 +3,9 @@ extern "C" {
 #include "wireless.h"
 }
 
-#include "RF24/RF24.h"
-#include "RF24Network/RF24Network.h"
-#include "RF24Mesh/RF24Mesh.h"
+#include <RF24.h>
+#include <RF24Network.h>
+#include <RF24Mesh.h>
 
 extern "C" {
 
